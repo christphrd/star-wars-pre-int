e96152bfe9e9e73aee4e657a5b8ea99a7eaebb5b
christphrd <diep.christopher@gmail.com>
2018-09-14 13:21:57 -0400

    http error handling. formatting date

--- src/App.js
+++ src/App.js
@@ -11,31 +11,26 @@ class App extends Component {
   state = {
     charSelected: "",
     filmLinks: [],
-    error: null,
     films: []
   }
 
   selectChar = (name, url) => {
     fetch(url)
     .then(res => res.json())
-    .then(json => this.setState({
-      charSelected: name,
-      filmLinks: json.films,
-      error: null,
-      films: []
-    }, this.fetchMovies(json.films)))
-    .catch(err => {
-      console.log(err)
-      this.setState({
-        charSelected: name,
-        filmLinks: [],
-        error: err,
-        films: []
-      })
+    .then(json => {
+      if (json.detail) {
+        alert(`Error: ${json.detail} (${url})`)
+      } else {
+        this.setState({
+          charSelected: name,
+          filmLinks: json.films,
+          films: []
+        }, this.fetchMovies(json.films))
+      }
     })
   }
 
-  fetchMovies = filmLinks => {
+  fetchMovies = (filmLinks = []) => {
     filmLinks.map(link => 
       fetch(link)
       .then(res => res.json())
@@ -52,7 +47,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Star Wars React</h1>
         </header>
-        <Card id="star-wars-card" raised="true">
+        <Card id="star-wars-card" raised={true}>
           <CardContent>
             <CharacterSelectionContainer selectChar={this.selectChar}/>
             {!!this.state.charSelected ? <MovieInfoContainer char={this.state} /> : null}
--- src/components/MovieData.js
+++ src/components/MovieData.js
@@ -3,12 +3,25 @@ import React from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
+
+
 class MovieData extends React.Component {
+    formatDate = d => {
+        let date = new Date(d);
+
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+        return `${weekdays[date.getUTCDay()]}, ${months[date.getUTCMonth()]} ${date.getUTCDate()} ${date.getUTCFullYear()}`
+    }
+
+
     render() {
         return (
             <TableRow>
               <TableCell>{this.props.film.title}</TableCell>
-              <TableCell>{this.props.film.release_date}</TableCell>
+              <TableCell>{this.formatDate(this.props.film.release_date)}</TableCell>
             </TableRow>
         )
     }
